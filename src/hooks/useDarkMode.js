9b6e69cca7e023889422750ee0909a01894cdee2
Tyler Kummer <tylerkummer@protonmail.com>
2020-03-11 17:46:40 -0500

    Initial Commit

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+
+export const useDarkMode = () => {
+    const [value, setValue] = useLocalStorage('dark-mode', true);
+
+    useEffect(() => {
+        if(useLocalStorage.value === true){
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    return [value, setValue];
+}
